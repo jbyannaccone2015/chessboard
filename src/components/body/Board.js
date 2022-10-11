cd40f3ecae9da3bc58057cbf67bbc1edb25bfc8b
Justin Yannaccone <justin@Josephs-MacBook-Pro-2.local>
2022-10-11 09:43:37 -0400

    Board skeleton added

---
+++ src/components/body/Board.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Board() {
+
+    return(
+        <div class="bg-gray-700 flex content-center items-center justify-center h-[100vh] text-slate-600">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
---
+++ src/components/body/Row.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Row() {
+    return(
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
